ccb1c5047d927c98ce1d72086f044482f3d10182
harsh makadiya <harsh.makadiya84@gmail.com>
2023-10-23 01:01:20 +0530

    new

--- 12_func/main.go
+++ 12_func/main.go
@@ -2,29 +2,20 @@ package main
 
 import "fmt"
 
-func add(a int, b int) int {
+func add(a int, b int) int { //1.two number addition
 	temt := 0
 	temt = a + b
 	return temt
 }
-
-// func squ(s int) int {
-// 	sq := 0
-// 	sq = s * s
-// 	return sq
-// }
-
-func squ(s int) int {
+func squ(s int) int { //2.square of number
 	return s * s
 }
-
-func swipping(s1, s2 *int) {
-	var temp int
-	temp = *s1
+func swipping(s1, s2 *int) { //3.swiping of number
+	// var temp int
+	temp := *s1
 	*s1 = *s2
 	*s2 = temp
 }
-
 func main() {
 	fmt.Println("1.addition of number")
 	fmt.Println("2.square of number")
@@ -60,12 +51,6 @@ func main() {
 		fmt.Printf("before swipping value is v1 %d v2 %d \n", swv1, swv2)
 		swipping(&swv1, &swv2)
 		fmt.Printf("before swipping value is v1 %d v2 %d", swv1, swv2)
-	// case 4:
-	// 	var num int
-	// 	fmt.Println("enter the number")
-	// 	fmt.Scanln(&num)
-
-	// 	for num=0
 
 	default:
 		fmt.Println("enterd invalid number")
